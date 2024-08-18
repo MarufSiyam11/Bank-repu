ef71863c4191504974812ca8c0ea8fce146528a3
marufsiyam <marufhasansiyam6@gmail.com>
2024-08-18 23:48:58 +0600

    added total deposit

--- js/deposit.js
+++ js/deposit.js
@@ -1,13 +1,26 @@
 // step 1 -add even listener to the deposit buttun
-document.getElementById('btn-diposit').addEventListener('click',function(){
-    // step-2 -get the dipositamouunt from the deposit input feild
-    const depositFeild =document.getElementById('deposit-feild')
-    const depositAmount =depositFeild.value;
-     
-    // step 3 -get the currect deposit total
-    const depositTotalElement =document.getElementById('deposit-total')
-        const depositTotal =depositTotalElement.innerText;
-        depositTotalElement.innerText=depositAmount;
-        depositFeild.value='';
-    
-})
+document.getElementById("btn-diposit").addEventListener("click", function () {
+  // step-2 -get the dipositamouunt from the deposit input feild
+  const depositFeild = document.getElementById("deposit-feild");
+  const newDepositAmountString = depositFeild.value;
+  const newDepositAmount = parseFloat(newDepositAmountString);
+  // step 3 -get the currect deposit total
+  const depositTotalElement = document.getElementById("deposit-total");
+  const previousDepositTotalString = depositTotalElement.innerText;
+  const previousDepositTotal = parseFloat(previousDepositTotalString);
+  // step 4 -add number to set the total deposit
+  const cuurentDepositTotal = previousDepositTotal + newDepositAmount;
+  // set the deposit total
+  depositTotalElement.innerText = cuurentDepositTotal;
+  // step 5- get balance current total
+
+  const balnceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balnceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  // step 6 - calculate current total balance
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  //   set the balance total
+  balnceTotalElement.innerText = currentBalanceTotal;
+  // step-7 -clear the depositfield
+  depositFeild.value = "";
+});
